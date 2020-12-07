67f09e7d80dfd30a7fcd9aa8ecf40f4bcef2921a
Rich <codeleap@outlook.com>
2020-12-07 18:29:45 +0100

    Quiz categories template tag created.

---
+++ miniquizapp/quizzes/templatetags/quiz_categories.py
@@ -0,0 +1,18 @@
+from django import template
+from quizzes.models import QuizCategory
+
+register = template.Library()
+
+@register.inclusion_tag("tags/get_latest_categories.html")
+def get_latest_categories():
+	all_categories = QuizCategory.objects.all()[:50]
+	return {
+	    'all_categories': all_categories
+	}
+
+@register.inclusion_tag("tags/get_all_categories.html")
+def get_all_categories():
+	all_categories = QuizCategory.objects.all()
+	return {
+	    'all_categories': all_categories
+	}
