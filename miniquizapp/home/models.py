06f11c2c2e4e654638c3882f72ea6c5d41392e48
Rich <codeleap@outlook.com>
2020-12-07 14:15:19 +0100

    Home model sessions added to context

--- miniquizapp/home/models.py
+++ miniquizapp/home/models.py
@@ -4,4 +4,11 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    def get_context(self, request):
+    	context = super().get_context(request)
+    	if 'most_recent_quiz_title' in request.session:
+    		context['most_recent_quiz_title'] = request.session['most_recent_quiz_title']
+    		context['most_recent_quiz_url'] = request.session['most_recent_quiz_url']
+    	return context
+
+
