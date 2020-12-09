c5d6fdfeee11b5aad30c1c3b3ed36649a6c59d48
Rich <codeleap@outlook.com>
2020-12-09 14:47:01 +0100

    Quizzes.js added.

---
+++ miniquizapp/quizzes/static/quizzes/js/quizzes.js
@@ -0,0 +1,14 @@
+/* 'QUIZ_PAGE.HTML' BUTTON AND DIV */
+/* Button reveals and hides quiz answers. */
+revealAnswersButton = document.getElementById("reveal-answers-button");
+quizAnswers = document.getElementById("quiz-answers");
+
+revealAnswersButton.onclick = function(){
+	if (quizAnswers.style.display === "block"){
+		quizAnswers.style.display = "none";
+		revealAnswersButton.innerHTML = "REVEAL ANSWERS!";
+	} else {
+		quizAnswers.style.display = "block";
+		revealAnswersButton.innerHTML = "HIDE ANSWERS!";
+	}
+}
