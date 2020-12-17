4558a1cd85b41b3540b6198e3a627b6441911511
Rich <codeleap@outlook.com>
2020-12-17 15:16:07 +0100

    Session vars now moved to custom content processor in quizzes

--- miniquizapp/home/models.py
+++ miniquizapp/home/models.py
@@ -4,11 +4,6 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    def get_context(self, request):
-    	context = super().get_context(request)
-    	if 'most_recent_quiz_title' in request.session:
-    		context['most_recent_quiz_title'] = request.session['most_recent_quiz_title']
-    		context['most_recent_quiz_url'] = request.session['most_recent_quiz_url']
-    	return context
+    pass
 
 
