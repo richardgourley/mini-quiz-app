74581ac9098616dcbac9fd631442351d62f5ac1c
Rich <codeleap@outlook.com>
2020-12-17 15:14:46 +0100

    Quizzes custom content processor added.

--- miniquizapp/miniquizapp/settings/base.py
+++ miniquizapp/miniquizapp/settings/base.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'quizzes.custom_content_processor.latest_quiz_visited_info',
             ],
         },
     },
