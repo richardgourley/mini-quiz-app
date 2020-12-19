23b2fc1ffec3433a29d6e4f69e2f220b160f9fb0
Rich <codeleap@outlook.com>
2020-12-19 15:13:31 +0100

    Timezone imported

--- miniquizapp/quizzes/tests/test_wagtail_pages.py
+++ miniquizapp/quizzes/tests/test_wagtail_pages.py
@@ -1,7 +1,11 @@
 # Inherits from django.test.TestCase
 from wagtail.tests.utils import WagtailPageTests
 from quizzes.models import QuizPage
+from home.models import HomePage
+from django.utils import timezone
 
+class HomePageTests(WagtailPageTests):
+	pass
 
 class QuizPageTests(WagtailPageTests):
     def test_can_create_a_page(self):
