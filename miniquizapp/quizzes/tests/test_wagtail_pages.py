0de975749f840c5ea61ae759fbe648e149d7f0e4
Rich <codeleap@outlook.com>
2020-12-19 16:16:17 +0100

    Homepage exists test added.

--- miniquizapp/quizzes/tests/test_wagtail_pages.py
+++ miniquizapp/quizzes/tests/test_wagtail_pages.py
@@ -5,7 +5,9 @@ from home.models import HomePage
 from django.utils import timezone
 
 class HomePageTests(WagtailPageTests):
-	pass
+	def test_home_page_exists(self):
+		home_page = HomePage.objects.all()[0]
+		self.assertEqual(home_page, 'Home')
 
 class QuizPageTests(WagtailPageTests):
     def test_can_create_a_page(self):
