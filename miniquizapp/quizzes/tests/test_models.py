0f093afeea2e01cef2c5ce8a1cd9e856427d9d28
Rich <codeleap@outlook.com>
2020-12-28 18:55:12 +0100

    QuizPage created, home page obtained from site root, QuizPage added as sub page of home

--- miniquizapp/quizzes/tests/test_models.py
+++ miniquizapp/quizzes/tests/test_models.py
@@ -1,11 +1,29 @@
 from django.test import TestCase
-from quizzes.models import QuizCategory
+from quizzes.models import QuizCategory, QuizPage
+from wagtail.core.models import Site
+
+from django.utils import timezone
 
 class QuizCategoryTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         # Non modified object used by all classes
         QuizCategory.objects.create(name="Sports", slug="sport")
+        '''
+        Get HOME page
+        '''
+        site = Site.objects.get(is_default_site=True)
+        home_page = site.root_page
+        '''
+        Create and add QUIZPAGE to HOME page
+        '''
+        quiz_page = QuizPage(
+            title='Tennis Quiz',
+            intro='An introduction to Quiz Test 1',
+            date=timezone.now(),
+        )
+        home_page.add_child(instance=quiz_page)
+
 
     def setUp(self):
         pass
@@ -27,6 +45,5 @@ class QuizCategoryTests(TestCase):
     def test_quiz_category_slug_is_unique(self):
         quiz_category = QuizCategory.objects.get(id=1)
         unique = quiz_category._meta.get_field('slug').unique
-        self.assertTrue(unique) 
-
+        self.assertTrue(unique)
 
