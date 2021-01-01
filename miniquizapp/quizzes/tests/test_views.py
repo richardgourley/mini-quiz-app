aed70d7b0d0b8d3781d0718b962cf7f04ed36e30
Rich <codeleap@outlook.com>
2021-01-01 17:35:15 +0100

    Initial QuizCategoryListViewTests created.

--- miniquizapp/quizzes/tests/test_views.py
+++ miniquizapp/quizzes/tests/test_views.py
@@ -1,2 +1,14 @@
 from django.test import TestCase, Client
-from quizzes.models import QuizIndexPage
+from quizzes.models import QuizIndexPage, QuizCategory
+from django.urls import reverse
+
+class QuizCategoryListViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Non modified object used by all classes
+        QuizCategory.objects.create(name="Sports", slug="sport")
+
+    def test_quiz_category_list_view_200(self):
+        self.client = Client()
+        response = self.client.get(reverse('quizzes:quiz_category_list_view'))
+        self.assertEqual(response.status_code, 200)
