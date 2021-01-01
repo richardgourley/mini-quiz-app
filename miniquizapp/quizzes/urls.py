da1cbc74a6a30d4c90f3deea3e37420e5f220021
Rich <codeleap@outlook.com>
2021-01-01 17:34:29 +0100

    Urls in quizzes namespaced.

--- miniquizapp/quizzes/urls.py
+++ miniquizapp/quizzes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'quizzes'
 urlpatterns = [
     path('category/<slug:slug>', views.QuizCategoryDetailView.as_view(), name="quiz_category_detail_view"),
     path('categories/', views.QuizCategoryListView.as_view(), name="quiz_category_list_view"),
