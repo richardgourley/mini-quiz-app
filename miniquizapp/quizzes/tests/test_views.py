c0f6b41cd97073ef8e8de42da74dfb0ead854cf2
Rich <codeleap@outlook.com>
2021-01-04 17:17:21 +0100

    QuizCategoryDetailViewTests class setup method added

--- miniquizapp/quizzes/tests/test_views.py
+++ miniquizapp/quizzes/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from quizzes.models import QuizIndexPage, QuizCategory
+from quizzes.models import QuizIndexPage, QuizCategory, QuizPage
 from django.urls import reverse
 
 class QuizCategoryListViewTests(TestCase):
@@ -40,5 +40,34 @@ class QuizCategoryListViewTests(TestCase):
     	all_categories = response.context['all_categories']
     	sports = all_categories.get(name='Sports')
     	self.assertTrue(sports.slug == 'sports')
+
+class QuizCategoryDetailViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Non modified object used by all classes
+        category = QuizCategory.objects.create(name="Geography", slug="geography")
+
+        '''
+        Get HOME page
+        '''
+        site = Site.objects.get(is_default_site=True)
+        home_page = site.root_page
+        '''
+        Create and add QUIZPAGE as sub page of HOME page
+        '''
+        quiz_page = QuizPage(
+            title='Capital Cities',
+            intro='A quiz about capital cities.',
+            date=timezone.now(),
+        )
+        home_page.add_child(instance=quiz_page)
+
+        # assign 'geography' category
+        quiz_page.categories.add(category)
+
+        # In shell, need to save the quiz_page to affect updated category change
+        # quiz_page.save()
+
+
 
 
