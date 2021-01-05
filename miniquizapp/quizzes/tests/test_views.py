54e3b6a2a8b9bb44c19e647f8abbf531a1534a09
Rich <codeleap@outlook.com>
2021-01-05 10:44:16 +0100

    QuizPageTests test class added

--- miniquizapp/quizzes/tests/test_views.py
+++ miniquizapp/quizzes/tests/test_views.py
@@ -105,6 +105,87 @@ class QuizCategoryDetailViewTests(TestCase):
         response = self.client.get(reverse('quizzes:quiz_category_detail_view', args=(geography.slug,)))
         self.assertTrue('<a href="/capital-cities/">' in str(response.content))
 
+class QuizPageTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Non modified object used by all classes
+        film_category = QuizCategory.objects.create(name="Films", slug="films")
+
+        site = Site.objects.get(is_default_site=True)
+        home_page = site.root_page
+
+        film_quiz_page = QuizPage(
+            title='Films from 1980-1990',
+            intro='A quiz about films between 1980 and 1990.',
+            date=timezone.now(),
+        )
+        home_page.add_child(instance=film_quiz_page)
+
+        # assign 'films' category
+        film_quiz_page.categories.add(film_category)
+
+        # QUIZ QUESTION
+        QuizQuestion.objects.create(
+            page=film_quiz_page,
+            question='Who directed the 1981 film the Raiders of the Lost Ark?',
+            answer='Steven Spielberg'
+        )
+
+    def test_film_quiz_page_200(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue(response.status_code == 200)
+
+    # CONTENT TESTS (CATEGORIES, BUTTONS, JS FILES)
+
+    def test_categories_appear(self):
+        response = self.client.get('/films-from-1980-1990/')
+        # test href appears for films category
+        self.assertTrue('href="/quizzes/category/films"' in str(response.content))
+        self.assertTrue('Films' in str(response.content))
+
+    def test_reveal_answers_button_appears(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('REVEAL ANSWERS!</button>' in str(response.content))
+
+    def test_js_quiz_page_loaded(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('<script type="text/javascript" src="/static/quizzes/js/quizzes.js"></script>' in str(response.content))
+
+    # CONTEXT TESTS ()
+
+    def test_page_exists_in_context(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('page' in response.context)
+    
+    def test_page_title(self):
+        response = self.client.get('/films-from-1980-1990/')
+        page = response.content['page']
+        self.assertEqual(page.title, 'Films from 1980-1990')
+
+    def test_page_slug(self):
+        response = self.client.get('/films-from-1980-1990/')
+        page = response.content['page']
+        self.assertEqual(page.slug, 'films-from-1980-1990')
+
+    
+    # CONTENT TESTS (QUESTION APPEARS TESTS)
+    
+    def test_film_quiz_page_title_appears(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('<h2>Films from 1980-1990</h2>' in str(response.content))
+
+    def test_quiz_intro_appears(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('A quiz about films between 1980 and 1990.' in str(response.content))
+
+    def test_question_appears(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('Who directed the 1981 film the Raiders of the Lost Ark?' in str(response.content))
+
+    def test_answer_appears(self):
+        response = self.client.get('/films-from-1980-1990/')
+        self.assertTrue('Steven Spielberg' in str(response.content))
+
 
 
 
